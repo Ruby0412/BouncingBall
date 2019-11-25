fc3f9884ac6e3e1c3da261beb83492dbb147977c
Mingqi Tan <ariel4795@gmail.com>
2019-11-25 08:03:00 +0800

    Add files via upload

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    //the main camera
+    public GameObject camera;
+    //main game controller(mainly about the map)
+    public GameObject GameController;
+    //score(based on block top surface touched)
+    public GameObject textScore;
+    //adjust the force index
+    public float adjustIndex = 1000f;
+    //int score
+    public int score = 0;
+
+    //get the script under game controll
+    private GameControl GameScript;
+    private Rigidbody ballRigidbody;
+    private Vector3 cam = new Vector3 (0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start () {
+        GameScript = GameController.GetComponent<GameControl> ();
+        ballRigidbody = GetComponent<Rigidbody> ();
+        //get the relative position norm
+        cam = camera.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void Jump (float pressTime) {
+        //add force for jumping based on direction and presstime
+        ballRigidbody.AddForce (new Vector3 (0, 3f, 0) + GameScript.direction * pressTime * adjustIndex, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter (Collision collision) {
+        //Debug.Log (collision.gameObject);
+        if (collision.gameObject.name == "Plane") {
+            GameScript.Ending ();
+        } else if (collision.gameObject != GameScript.currentBlock && collision.gameObject.tag == "box" && GameScript.gameEnd == false) {
+            score++;
+            textScore.GetComponent<UnityEngine.UI.Text> ().text = score + "";
+            StartCoroutine (FollowBall (transform.position + cam, 2.5f));
+            GameScript.currentBlock = collision.gameObject;
+            GameScript.MakeBlock ();
+        }
+
+    }
+
+    //move the camera to the position of next block the ball standing
+    IEnumerator FollowBall (Vector3 end, float speed) {
+        while (Vector3.Distance (camera.transform.position, end) > speed * Time.deltaTime) {
+            camera.transform.position = Vector3.MoveTowards (camera.transform.position, end, speed * Time.deltaTime);
+            yield return 0;
+        }
+        camera.transform.position = end;
+    }
+
+}
---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    //the bouncing ball
+    public GameObject ball;
+    //the starting block
+    public GameObject block;
+    //reset button on Canvas that shows up when game ends
+    public GameObject resetButton;
+    //particle effect when pressed
+    public GameObject pressParticle;
+    //update the current standing block
+    public GameObject currentBlock;
+    //customizable blocks
+    public GameObject[] BlockPrefabs;
+    //set to true when ball touches the plane
+    public bool gameEnd;
+    //respawn direction 
+    public Vector3 direction = new Vector3 (0, 0, 0);
+
+    //pressTime = old time point - current time
+    private float pressTime;
+    private float startTime;
+    //used to restore block shapes from squeezing
+    private Vector3 originalScale = new Vector3 (0, 0, 0);
+    private Vector3 newScale = new Vector3 (0, 0, 0);
+    //get access to ball variables and functions
+    private Ball BallScript;
+    private Rigidbody ballRigidbody;
+    private int indexRandom;
+    private float random;
+    private GameObject spawn;
+    private GameObject newBlock;
+   
+
+    // Start is called before the first frame update
+    void Start () {
+        //making the next block to jump on
+        MakeBlock ();
+        //set current block to start block
+        currentBlock = block;
+        //hide reset
+        resetButton.SetActive(false);
+        pressParticle.SetActive (false);
+
+        BallScript = ball.GetComponent<Ball>();
+        originalScale = currentBlock.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!gameEnd) {
+            if (Input.GetMouseButtonDown (0)) {
+                startTime = Time.time;
+                pressParticle.SetActive (true);
+            }
+            if (Input.GetMouseButtonUp (0)) {
+                pressParticle.SetActive (false);
+                //calculate presstime
+                pressTime = Time.time - startTime;
+                BallScript.Jump (pressTime);
+                currentBlock.transform.localScale = originalScale;
+                originalScale = newScale;
+            }
+            if (Input.GetMouseButton (0)) {
+                //squeeze the block by y-axis
+                if (currentBlock.transform.localScale != Vector3.zero && currentBlock.transform.localScale.y > 0.03) {
+                    currentBlock.transform.localScale += new Vector3 (0, -1, 0) * 0.15f * Time.deltaTime;
+                }
+
+            }
+        }
+
+    }
+
+    /* This function is used to make the next block with different color, size and distance */
+    public void MakeBlock () {
+        if (!gameEnd) {
+            //random the direction to go either left or right
+            indexRandom = Random.Range (0, 2);
+            if (indexRandom == 0) {
+                direction = new Vector3 (1, 0, 0);
+            } else {
+                direction = new Vector3 (0, 0, 1);
+            }
+
+            //choose the next model randomize from array of blocks
+            spawn = BlockPrefabs[Random.Range (0, BlockPrefabs.Length)];
+            
+            newBlock = Instantiate (spawn);
+            newBlock.transform.position = currentBlock.transform.position + direction * Random.Range (0.35f, 0.5f);
+            random = Random.Range (0.15f, 0.22f);
+            newBlock.transform.localScale = new Vector3 (random, 0.1f, random);
+            newScale = newBlock.transform.localScale;
+
+            newBlock.GetComponent<Renderer> ().material.color = new Color (Random.Range (0f, 1), Random.Range (0f, 1), Random.Range (0f, 1));
+        }
+    }
+
+    /* resetButton */
+    public void Ending () {
+        gameEnd = true;
+        resetButton.SetActive(true);
+    }
+
+    public void Reset () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+    }
+}
